faf294102fed7d84d22bfb4ae849789f2b2a6757
Baradel-Clement <baradelclement@gmail.com>
2022-11-29 11:45:16 +0100

    .

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: true,
+})
+
+module.exports = withBundleAnalyzer(nextConfig)
